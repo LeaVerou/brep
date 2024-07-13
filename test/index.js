3ae77980579ca3f37aee231e3310e8b168886b59
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-07-14 00:51:20 +0200

    Add tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,12 @@
 import compile from "./compile.js";
 import replace from "./replace.js";
+import {default as util} from "./util.js";
 
 export default {
 	name: "All tests",
 	tests: [
 		compile,
 		replace,
+		util,
 	]
 }
